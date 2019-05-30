922b2475f0e57a7a4a4fe7196844aeb529188b56
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2019-05-30 15:11:34 -0300

    aplicando JpaRepository

--- src/test/java/com/guitar/db/ModelTypePersistenceTests.java
+++ src/test/java/com/guitar/db/ModelTypePersistenceTests.java
@@ -1,10 +1,7 @@
 package com.guitar.db;
 
-import static org.junit.Assert.assertEquals;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
+import com.guitar.db.model.ModelType;
+import com.guitar.db.repository.ModelTypeJpaRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,38 +9,48 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.guitar.db.model.ModelType;
-import com.guitar.db.repository.ModelTypeRepository;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
 
-@ContextConfiguration(locations={"classpath:com/guitar/db/applicationTests-context.xml"})
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@ContextConfiguration(locations = {"classpath:com/guitar/db/applicationTests-context.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ModelTypePersistenceTests {
-	@Autowired
-	private ModelTypeRepository modelTypeRepository;
+    @Autowired
+    private ModelTypeJpaRepository modelTypeJpaRepository;
 
-	@PersistenceContext
-	private EntityManager entityManager;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-	@Test
-	@Transactional
-	public void testSaveAndGetAndDelete() throws Exception {
-		ModelType mt = new ModelType();
-		mt.setName("Test Model Type");
-		mt = modelTypeRepository.create(mt);
-		
-		// clear the persistence context so we don't return the previously cached location object
-		// this is a test only thing and normally doesn't need to be done in prod code
-		entityManager.clear();
+    @Test
+    public void testJpaFind() {
+        List<ModelType> modelTypes = modelTypeJpaRepository.findAll();
+        assertNotNull(modelTypes);
+    }
 
-		ModelType otherModelType = modelTypeRepository.find(mt.getId());
-		assertEquals("Test Model Type", otherModelType.getName());
-		
-		modelTypeRepository.delete(otherModelType);
-	}
+    @Test
+    @Transactional
+    public void testSaveAndGetAndDelete() throws Exception {
+        ModelType mt = new ModelType();
+        mt.setName("Test Model Type");
+        mt = modelTypeJpaRepository.save(mt);
 
-	@Test
-	public void testFind() throws Exception {
-		ModelType mt = modelTypeRepository.find(1L);
-		assertEquals("Dreadnought Acoustic", mt.getName());
-	}
+        // clear the persistence context so we don't return the previously cached location object
+        // this is a test only thing and normally doesn't need to be done in prod code
+        entityManager.clear();
+
+        ModelType otherModelType = modelTypeJpaRepository.findOne(mt.getId());
+        assertEquals("Test Model Type", otherModelType.getName());
+
+        modelTypeJpaRepository.delete(otherModelType);
+    }
+
+    @Test
+    public void testFind() throws Exception {
+        ModelType mt = modelTypeJpaRepository.findOne(1L);
+        assertEquals("Dreadnought Acoustic", mt.getName());
+    }
 }
