36f1a623851c76faf9fe56fbebb69cccdb9b7c0e
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2019-05-30 16:59:32 -0300

    Anotar com @Repository, as interfaces JpaRepository, não é necessário porém melhora e reforça o entendimento do código.

--- src/main/java/com/guitar/db/repository/ManufactureJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ManufactureJpaRepository.java
@@ -2,6 +2,8 @@ package com.guitar.db.repository;
 
 import com.guitar.db.model.Manufacturer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ManufactureJpaRepository extends JpaRepository<Manufacturer, Long> {
 }
